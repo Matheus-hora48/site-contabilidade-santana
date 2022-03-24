6ee6814724ccd91c0c39467d1f34b18393b7c464
Matheus Hora <matheus11hora@gmail.com>
2022-03-24 15:28:46 -0300

    Novos

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-<a href="contato">contato</a>
 
 <?php
 
 $rota = explode("/", $_GET['url'] ?? 'home');
 
-var_dump($rota);
 
 if (file_exists("pages/{$rota[0]}.html")){
     include "pages/{$rota[0]}.html";
