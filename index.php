32a6410646321c3545b9c602ab437bfef393df27
Matheus Hora <matheus11hora@gmail.com>
2022-03-24 10:52:51 -0300

    Novos

--- index.php
+++ index.php
@@ -493,3 +493,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </body>
 </html>
+
+<?php
+
+$rota = $_GET['url'] ?? 'index'
+
+
+?>
